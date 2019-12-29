852e959ea28fa2c93f1ba12588e4d1eb9263021e
yumauma20 <71yuma10@gmail.com>
2019-12-29 15:38:45 +0900

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    実験
                 </div>
 
                 <div class="links">
