04308ba84607bd687016dac782c7f599902392f4
yumauma20 <71yuma10@gmail.com>
2019-12-30 23:26:57 +0900

    タスク一覧表示

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,17 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index(int $id)
-{
-    $folders = Folder::all();
-
-    return view('tasks/index', [
-        'folders' => $folders,
-        'current_folder_id' => $id,
-    ]);
-}
+    {
+        // すべてのフォルダを取得する
+        $folders = Folder::all();
+    
+        // 選ばれたフォルダを取得する
+        $current_folder = Folder::find($id);
+    
+        // 選ばれたフォルダに紐づくタスクを取得する
+        $tasks = Task::where('folder_id', $current_folder->id)->get();
+    
+        return view('tasks/index', [
+            'folders' => $folders,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
+        ]);
+    }
 }
